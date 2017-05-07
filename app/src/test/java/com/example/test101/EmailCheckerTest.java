78ee9e8a65ba3b24a2d39d92d9aa8760c0d8a968
pankaj.nimgade <pankaj.nimgade@gmail.com>
2017-05-06 20:18:31 -0400

    added mockito to provide android "test needs to interact with Android dependencies"

---
+++ app/src/test/java/com/example/test101/EmailCheckerTest.java
@@ -0,0 +1,16 @@
+package com.example.test101;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Heidi on 5/6/2017.
+ */
+
+public class EmailCheckerTest {
+
+    @Test
+    public void email_Test(){
+        Assert.assertArrayEquals(new char[]{'a'},new char[]{'a'});
+    }
+}
